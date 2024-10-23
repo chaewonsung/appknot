2868d03d041ef64bd9ddff8439a0bcf8e5a668e1
Hwang Chae Won <codnjs2445@naver.com>
2024-10-23 18:27:58 +0900

    Updates

--- src/js/utils/toPX.js
+++
@@ -1,6 +0,0 @@
-export default function toPX(value) {
-  return (
-    (parseFloat(value) / 100) *
-    (/vh/gi.test(value) ? window.innerHeight : window.innerWidth)
-  );
-}
--- src/js/utils/toggleClassWithIo.js
+++
@@ -1,9 +0,0 @@
-export default function toggleClassWithIo(targetNode, className, options) {
-  new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.intersectionRatio) {
-        targetNode.classList.toggle(className);
-      }
-    });
-  }, options).observe(targetNode);
-}
